b319219bb2e33a223315c506d707e9adfee541b4
Debtanu31mondal <106553665+Debtanu31mondal@users.noreply.github.com>
2022-06-24 08:22:56 +0530

    Update InheritanceActivity.java

--- InheritanceActivity.java
+++ InheritanceActivity.java
@@ -30,7 +30,7 @@ class Employees {
         }
     } // end of method calculateTransportAllowance
 }
-class Manager extends Employees // creating sub class manager (Hirarchical inheritance)
+class Manager extends Employees // creating sub class manager (Hierarchical inheritance)
 {
     public Manager(long Id,String name,String address,long phone,double salary) // subclass constructor (Constructor overloading)
     {
@@ -52,10 +52,10 @@ class Trainee extends Employees{ // another sub class for Trainee (Hirarchical i
 }
 public class InheritanceActivity {
     public static void main(String[] args) { // main method
-        Manager manager=new Manager(22334455,"Debtanu","Howrah",26552958,25000); // object creation in Manager class
+        Manager manager=new Manager(126534,"Peter","Chennai India",237844,65000); // object creation in Manager class
         manager.calculatesalary();// method invoking to calculate salary of the manager
         manager.calculateTransportAllowance(); // method invoking to calculate transport allowance of the manager
-        Trainee trainee=new Trainee(123456,"Amit","Kolkata",5486875,20000); // object creation in the Trainee class
+        Trainee trainee=new Trainee(29846,"Jack","Mumbai India",442085,45000); // object creation in the Trainee class
         trainee.calculatesalary(); // method invoking to calculate salary of the trainee
         trainee.calculateTransportAllowance(); // method invoking to calculate transport allowance of the trainee
     } // end of main method
